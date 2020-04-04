c9f034fd236dfa30be21d616519f1bf1c369544e
oyebayofemi <61966651+oyebayofemi@users.noreply.github.com>
2020-04-04 10:50:09 +0100

    PHP login vaildation without database

---
+++ dashboard.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <style>
+            h1{
+                color:green;
+            }
+        </style>
+    </head>
+
+    <p > <h1 align ="center" color>You have logged in</h1></p>
+    <?php
+        echo "<a href='logout.php'>Logout</a>";
+    ?>
+
+</html>
+
